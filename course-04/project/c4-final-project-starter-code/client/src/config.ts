7e292924abe22f1b4e697d21be76aedd72536497
Anthony Oyathelemhi <tonero91@gmail.com>
2022-11-15 02:45:30 +0100

    update client config values

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -2,7 +2,7 @@ const apiId = '7q6ovo1lh2'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-suwbuavnap22t72z.us.auth0.com', // Auth0 domain
-  clientId: 'zFUafh09ZvfhAwyPLQ6J4xZmFACNqc9k', // Auth0 client id
+  domain: 'udacity-frontendtony.us.auth0.com', // Auth0 domain
+  clientId: 'CsMTu5d0xod9BSLvnwNEc3Y0WFgicDnZ', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
